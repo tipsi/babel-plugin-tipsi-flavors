41124e3cbf9c4c6f9bc2b74cb6993f6f8f2dae47
Anton Kuznetsov <isnifer@gmail.com>
2016-12-23 18:55:13 +0300

    Fix Example2.tipsi component

--- example/src/components/Example2.tipsi.js
+++ example/src/components/Example2.tipsi.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image } from 'react-native'
 
 export default class Example2Tipsi extends Component {
   render() {
