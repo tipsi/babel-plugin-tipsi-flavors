f5ff2c06cb3b67534c4388d617df4eb54ebbbf2f
Anton Kuznetsov <isnifer@gmail.com>
2016-12-26 19:58:17 +0300

    Update ci.sh

--- .travis/ci.sh
+++ .travis/ci.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
+FLAVORS=custom,tipsi npm test
+cd example_tmp
+
 case "${TRAVIS_OS_NAME}" in
   osx)
-    npm test
-    cd example_tmp
     set -o pipefail && FLAVORS=custom,tipsi npm run build:ios | xcpretty -c -f `xcpretty-travis-formatter`
     npm run test:ios
-    cd ../
   ;;
   linux)
-    npm test
-    cd example_tmp
     FLAVORS=custom,tipsi npm run build:android
     npm run test:android
-    cd ../
   ;;
 esac
